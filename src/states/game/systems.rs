928ee3620699ce198912a45b1633502b73d061d9
Mylah Dee <vixeliz@icloud.com>
2023-01-22 00:58:25 -0600

    Pathfinding working

--- src/states/game/systems.rs
+++ src/states/game/systems.rs
@@ -759,11 +759,17 @@ pub fn main_enemy_move(
                 }
             }
         }
+        let backup_vector_path: Vec<[i32; 2]> = vec![[0, 0]];
         enemy_path.0 = astar_map
             .astar
             .find_path(&path_map.path_map, enemy_location, [8, 18])
-            .unwrap()
+            .unwrap_or_else(|| &&backup_vector_path)
             .to_vec();
+        astar_map.astar.clear();
+        astar_map.astar = AStar::from_size([
+            path_map.path_map.width().try_into().unwrap(),
+            path_map.path_map.height().try_into().unwrap(),
+        ]);
     }
 }
 
@@ -774,8 +780,8 @@ fn convert_world_to_grid(level_location: &Vec3, level_size: &Vec2, target: &Vec2
         && target.y < level_location.y + level_size.y
     {
         return [
-            ((target.x - level_location.x) / 16.) as i32,
-            ((target.y - level_location.y) / 16.) as i32,
+            ((target.x - level_location.x).abs() / 16.) as i32,
+            ((target.y - level_location.y).abs() / 16.) as i32,
         ];
     } else {
         return [0, 0];
